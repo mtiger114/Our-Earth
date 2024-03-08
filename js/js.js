6d85305291b682ec50826c5915e8ee3e6660f617
Mahmoud Fahmy <105823344+Ma7moudTiger@users.noreply.github.com>
2024-03-08 21:06:12 +0200

    new

---
+++ js/js.js
@@ -0,0 +1,39 @@
+// Function to open overlay when a box is clicked
+var boxes = document.querySelectorAll(".box");
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    document.getElementById("overlay").style.display = "block";
+  });
+});
+
+// Function to close overlay
+function closeOverlay() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+
+
+  // Function to open search overlay
+  function openSearchOverlay() {
+    document.getElementById('searchOverlay').style.display = 'block';
+    document.getElementById('searchInput').focus(); // Focus on search input field
+  }
+
+  // Function to close search overlay
+  function closeSearchOverlay() {
+    document.getElementById('searchOverlay').style.display = 'none';
+  }
+
+  // Function to search text in the page
+  document.getElementById('searchInput').addEventListener('input', function() {
+    var searchQuery = this.value.toLowerCase();
+    var elements = document.querySelectorAll('body *');
+
+    var searchResults = [];
+    elements.forEach(function(element) {
+      if (element.textContent.toLowerCase().includes(searchQuery)) {
+        searchResults.push(element.outerHTML);
+      }
+    });
+  });
+
